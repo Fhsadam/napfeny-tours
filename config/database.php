0f7ffa9405204548ff67c3956dde28c078f22830
tuDavid <59034050+Davidka078HUN@users.noreply.github.com>
2026-04-18 23:22:25 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,9 +5,9 @@ $driver = getenv('DB_DRIVER') ?: 'mysql';
 return [
     'driver' => $driver,
     'host' => getenv('DB_HOST') ?: 'localhost',
-    'dbname' => getenv('DB_NAME') ?: 'adatb',
-    'username' => getenv('DB_USER') ?: 'adatbf',
-    'password' => getenv('DB_PASS') ?: 'jelszo',
+    'dbname' => getenv('DB_NAME') ?: 'utazas',
+    'username' => getenv('DB_USER') ?: 'fhsdsql',
+    'password' => getenv('DB_PASS') ?: 'FhsDWeb123.',
     'charset' => 'utf8mb4',
     'sqlite_path' => __DIR__ . '/../storage/napfeny.sqlite',
     'json_path' => __DIR__ . '/../storage/database.json',
