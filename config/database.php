b2910cf5c516cd042c0434833ae240ed50ba13f8
Festő-Hegedűs Sándor Ádám <m8xuer@example.local>
2026-04-21 21:32:35 +0200

    Friss projekt feltoltese

--- config/database.php
+++ config/database.php
@@ -4,10 +4,10 @@ $driver = getenv('DB_DRIVER') ?: 'json';
 
 return [
     'driver' => $driver,
-    'host' => getenv('DB_HOST') ?: 'localhost',
-    'dbname' => getenv('DB_NAME') ?: 'adatb',
-    'username' => getenv('DB_USER') ?: 'adatbf',
-    'password' => getenv('DB_PASS') ?: 'jelszo',
+    'host' => getenv('DB_HOST') ?: 'localhost:3306',
+    'dbname' => getenv('DB_NAME') ?: 'zap1380365_',
+    'username' => getenv('DB_USER') ?: 'fhsadam',
+    'password' => getenv('DB_PASS') ?: 'Fhsadam123!',
     'charset' => 'utf8mb4',
     'sqlite_path' => __DIR__ . '/../storage/napfeny.sqlite',
     'json_path' => __DIR__ . '/../storage/database.json',
